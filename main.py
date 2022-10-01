00beff9e9520258bddd5aad10f9ef641868fbd58
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-10-01 09:27:21 -0300

    Database start

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import sqlite3
+
+from knearest.knearest import KNearest
+
+
 def main():
-    ...
+    con = sqlite3.connect("db.db")
+    cur = con.cursor()
+
+    fruits = cur.execute("SELECT * FROM fruits")
+
+    knn = KNearest([(fruit[0], fruit[1:]) for fruit in fruits])
+    print(knn.get_data_points())
 
 
 if __name__ == '__main__':
