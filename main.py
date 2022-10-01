90f2fcd3fa4c48c6f4d995db127b7f87b2ebffba
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-10-01 13:20:13 -0300

    Close database when program ends

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from knearest.knearest import KNearest
 
 
 def main():
-    con = sqlite3.connect("db.db")
-    cur = con.cursor()
+    with sqlite3.connect('db.db') as con:
+        cur = con.cursor()
+        fruits = cur.execute("SELECT * FROM fruits")
 
-    fruits = cur.execute("SELECT * FROM fruits")
-
-    knn = KNearest([(fruit[0], fruit[1:]) for fruit in fruits])
-    print(knn.get_data_points())
+        knn = KNearest([(fruit[0], fruit[1:]) for fruit in fruits])
+        print(knn.get_data_points())
 
 
 if __name__ == '__main__':
